f859d3a4cf23f464c0d0c2abd2e5f9dd68898d79
Antonio <a.henrique1997@gmail.com>
2019-08-06 19:38:24 -0300

    introducao react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import profile from "./assets/agostinho-carrara.jpg";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="250" src={profile} />;
+  return <TechList />;
 }
 
 export default App;
