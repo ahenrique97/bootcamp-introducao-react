ec03fa3a1117700cf6fcb1e6802d9f07d5dd5009
Antonio <a.henrique1997@gmail.com>
2019-08-06 19:09:34 -0300

    introducao react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+import profile from "./assets/agostinho-carrara.jpg";
+
+function App() {
+  return <img width="250" src={profile} />;
+}
+
+export default App;
